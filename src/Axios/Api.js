fc452ff919702000d510ea46636f6233e2a17628
Ayoub El mssellek <ayoubmssallak7@gmail.com>
2025-06-02 23:21:18 +0200

    url update

--- src/Axios/Api.js
+++ src/Axios/Api.js
@@ -1,22 +1,14 @@
 import axios from 'axios';
 
 const Api = axios.create({
-baseURL: '/api', // كل الطلبات تبدأ من /api (عشان البروكسي في dev أو التوجيه في production)
-withCredentials: true,
-headers: {
-'Accept': 'application/json',
-'Content-Type': 'application/json',
-},
+  baseURL: import.meta.env.DEV
+    ? '/api'
+    : 'https://laravelapi-production-64b8.up.railway.app/api',
+  withCredentials: true,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
 });
-
-// هنا تقدر تضيف Interceptors لو تبغى (مثلا إضافة توكن بشكل أوتوماتيكي)
-
-// api.interceptors.request.use(config => {
-//   const token = localStorage.getItem('token');
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
-//   return config;
-// });
 
 export default Api;
